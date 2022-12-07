022a473d88d0365c36d61f76aa1c235369b643c7
c1ntani <cintani_muljono@hotmail.com>
2022-12-07 15:37:51 +0530

    Integrate API Server utilities and Development utilities.

---
+++ src/api/models/db.js
@@ -0,0 +1,9 @@
+const sqlite3 = require('sqlite3');
+
+const connection = new sqlite3.Database(process.cwd()+"/database/test.sqlite", sqlite3.OPEN_READWRITE, (err, res) => {
+  if(err){
+    return console.error(err.message);
+  }
+})
+
+module.exports = connection;
